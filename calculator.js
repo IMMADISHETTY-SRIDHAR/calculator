b8e43a09c9de58cdd162be0a57f9b9c4d7d8c3a7
sridhar immadishetty <food-munch>
2024-10-07 16:08:11 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,65 @@
+const result = document.getElementById('result');
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
+function performCalculation(previousNum, op, currentNum) {
+  let num1 = parseFloat(previousNum);
+  let num2 = parseFloat(currentNum);
+  let result;
+
+  switch (op) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 === 0) {
+        return "Error: Division by zero";
+      }
+      result = num1 / num2;
+      break;
+    default:
+      return "Error: Invalid operator";
+  }
+
+  return result.toString();
+}
+
+function appendNumber(number) {
+  currentNumber += number;
+  result.value = currentNumber;
+}
+
+function appendOperator(op) {
+  if (currentNumber !== "") {
+    previousNumber = currentNumber;
+    currentNumber = "";
+    operator = op;
+  }
+}
+
+function clearScreen() {
+  currentNumber = "";
+  previousNumber = "";
+  operator = "";
+  result.value = "";
+}
+
+function calculate() {
+  if (currentNumber !== "" && operator !== "") {
+    try {
+      result.value = performCalculation(previousNumber, operator, currentNumber);
+      previousNumber = result.value;
+      currentNumber = "";
+      operator = "";
+    } catch (err) {
+      result.value = err;
+    }
+  }
+}
